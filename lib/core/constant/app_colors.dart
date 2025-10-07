28ed0c3f12a281c4f2a67f0e29dff4dd36f8a383
mohamed-zeytoun123 <bdrabw74@gmail.com>
2025-10-07 13:53:20 +0300

    Add core folder structure

---
+++ lib/core/constant/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  //?-----------------------------------------------------
+
+  //* Primary Colors
+  static const Color background = Color(0xffFFFFFF);
+
+  //?-----------------------------------------------------
+}
---
+++ lib/features/auth/presentation/pages/log_in_page.dart
@@ -0,0 +1,11 @@
+import 'package:e_learning/core/constant/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class LogInPage extends StatelessWidget {
+  const LogInPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: AppColors.background);
+  }
+}
